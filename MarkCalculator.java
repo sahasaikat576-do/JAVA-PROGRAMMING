0895314af9577537a5ef4f391ae3365b2f3476ba
sahasaikat576-do <sahasaikat576@gmail.com>
2025-02-21 11:03:25 +0530

    Add files via upload

---
+++ MarkCalculator.java
@@ -0,0 +1,17 @@
+ class Markscalculator {
+    public static void main(String[] args) {
+        // Marks in each subject
+        int mathsMark = 94;
+        int physicsMark = 95;
+        int chemistryMark = 96;
+
+        // Calculate total marks and average
+        int totalMarks = mathsMark + physicsMark + chemistryMark;
+        double averageMark = totalMarks / 3.0; // Dividing by 3 to get the average
+
+        // Output the result
+        System.out.println("Sam's average mark in PCM is " + averageMark);
+    }
+}
+
+	
